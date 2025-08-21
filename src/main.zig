e86048eb66577c6ab4c13d89b3f6b74a158ec367
Ibrahim Musaddequr Rahman <iamr@umich.edu>
2025-08-21 10:03:56 -0400

    added grid highlghting and tracking

--- src/main.zig
+++ src/main.zig
@@ -18,6 +18,7 @@ const ScreenPos = struct {
 fn printGrid(inc: i32, start: i32, end: i32) void {
     var i: i32 = start;
 
+    // only works with square resolution rn
     while (i < end) : (i += inc) {
         // vertical line
         rl.DrawLine(i, start, i, end, rl.GRAY);
@@ -26,14 +27,21 @@ fn printGrid(inc: i32, start: i32, end: i32) void {
     }
 }
 
+fn getSquareInGrid(gridSize: i32, x: i32, y: i32) ScreenPos {
+    const leftX = @divFloor(x, gridSize) * gridSize;
+    const topY = @divFloor(y, gridSize) * gridSize;
+    return .{ .x = leftX, .y = topY };
+}
+
 pub fn main() !void {
     rl.SetTraceLogLevel(rl.LOG_DEBUG);
 
     // important on macOS
     rl.SetConfigFlags(rl.FLAG_WINDOW_HIGHDPI);
+    rl.SetConfigFlags(rl.FLAG_WINDOW_RESIZABLE);
 
-    const screenWidth: i32 = 800;
-    const screenHeight: i32 = 450;
+    const screenWidth: i32 = 1000;
+    const screenHeight: i32 = 1000;
     rl.InitWindow(screenWidth, screenHeight, "rts engine testing");
     defer rl.CloseWindow();
 
@@ -49,26 +57,21 @@ pub fn main() !void {
     const centerY: i32 = screenHeight / 2;
 
     // consider creating a struct for this
-    var targetPt: ScreenPos = .{ .x = centerX, .y = centerY };
+    var clickedPt: ScreenPos = .{ .x = centerX, .y = centerY };
     var agentPt: ScreenPos = .{ .x = centerX, .y = centerY };
 
-    var gridSize: i32 = 10;
+    var gridSize: i32 = 50;
     const gridChange: i32 = 10;
+
+    std.debug.assert(@mod(gridSize, 2) == 0);
+    std.debug.assert(@mod(gridChange, 2) == 0);
 
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
         defer rl.EndDrawing();
         rl.ClearBackground(rl.RAYWHITE);
 
-        // scroll grid scaling is not great
-        // if (rl.GetMouseWheelMove() > 0) {
-        //     if (rl.GetMouseWheelMoveV().y > 0) {
-        //         gridSize += @intFromFloat(rl.GetMouseWheelMoveV().y);
-        //     }
-        //     else {
-        //         gridSize -= @intFromFloat(rl.GetMouseWheelMoveV().y);
-        //     }
-        // }
+        // scale grid
         if (rl.IsKeyPressed(rl.KEY_LEFT)) {
             gridSize -= gridChange;
         } else if (rl.IsKeyPressed(rl.KEY_RIGHT))
@@ -77,26 +80,32 @@ pub fn main() !void {
         gridSize = @max(gridSize, 5);
         gridSize = @min(gridSize, 100);
 
-        printGrid(gridSize, gridSize, screenWidth);
-
         if (rl.IsMouseButtonPressed(rl.MOUSE_LEFT_BUTTON)) {
-            targetPt.x = rl.GetMouseX();
-            targetPt.y = rl.GetMouseY();
+            clickedPt.x = rl.GetMouseX();
+            clickedPt.y = rl.GetMouseY();
         }
 
-        if (agentPt.x < targetPt.x) {
+        // highlight square of grid containing target
+        const gridSquare = getSquareInGrid(gridSize, clickedPt.x, clickedPt.y);
+        const gridSquareCenter = .{ .x = gridSquare.x + @divFloor(gridSize, 2), .y = gridSquare.y + @divFloor(gridSize, 2) };
+
+        if (agentPt.x < gridSquareCenter.x) {
             agentPt.x += vel;
-        } else if (agentPt.x > targetPt.x) {
+        } else if (agentPt.x > gridSquareCenter.x) {
             agentPt.x -= vel;
         }
 
-        if (agentPt.y < targetPt.y) {
+        if (agentPt.y < gridSquareCenter.y) {
             agentPt.y += vel;
-        } else if (agentPt.y > targetPt.y) {
+        } else if (agentPt.y > gridSquareCenter.y) {
             agentPt.y -= vel;
         }
 
-        rl.DrawCircle(targetPt.x, targetPt.y, 5, rl.LIGHTGRAY);
+        rl.DrawRectangle(gridSquare.x, gridSquare.y, gridSize, gridSize, rl.GREEN);
+
+        printGrid(gridSize, 0, @max(screenWidth, screenHeight));
+
+        // rl.DrawCircle(clickedPt.x, clickedPt.y, 5, rl.LIGHTGRAY);
         rl.DrawCircle(agentPt.x, agentPt.y, 10, rl.RED);
     }
 }
