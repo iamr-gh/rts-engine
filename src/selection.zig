83c4013180122b003598474142a4677a5456ab43
Ibrahim Musaddequr Rahman <iamr@umich.edu>
2025-12-31 22:29:03 -0500

    fixed rectangle selection to work in the way I wanted it

--- src/selection.zig
+++ src/selection.zig
@@ -8,34 +8,47 @@ const rl = grid.rl;
 pub const Box = struct {
     src: types.ScreenPos = types.ScreenPos{},
     dst: types.ScreenPos = types.ScreenPos{},
+    active: bool = false,
 };
 
 pub fn insideBox(pos: types.ScreenPos, box: Box) bool {
-    return pos.x >= box.src.x and pos.x <= box.dst.x and pos.y >= box.src.y and pos.y <= box.dst.y;
+    const minX = @min(box.src.x, box.dst.x);
+    const maxX = @max(box.src.x, box.dst.x);
+    const minY = @min(box.src.y, box.dst.y);
+    const maxY = @max(box.src.y, box.dst.y);
+
+    return pos.x >= minX and pos.x <= maxX and pos.y >= minY and pos.y <= maxY;
 }
 
 pub fn updateBox(box: *Box) void {
-    if (rl.IsMouseButtonDown(rl.MOUSE_LEFT_BUTTON)) {
-        if (box.src.x == 0 and box.src.y == 0) {
-            box.src.x = rl.GetMouseX();
-            box.src.y = rl.GetMouseY();
-        } else {
-            box.dst.x = rl.GetMouseX();
-            box.dst.y = rl.GetMouseY();
-        }
+    if (rl.IsMouseButtonPressed(rl.MOUSE_LEFT_BUTTON)) {
+        box.src.x = rl.GetMouseX();
+        box.src.y = rl.GetMouseY();
+        box.dst = box.src;
+        box.active = true;
+    } else if (rl.IsMouseButtonDown(rl.MOUSE_LEFT_BUTTON) and box.active) {
+        box.dst.x = rl.GetMouseX();
+        box.dst.y = rl.GetMouseY();
     } else {
-        // box.topLeft = types.ScreenPos{};
-        // box.bottomRight = types.ScreenPos{};
+        box.active = false;
     }
 }
 
 pub fn drawBox(box: Box) void {
-    if (box.src.x == box.dst.x or box.src.y == box.dst.y) {
+    if (!box.active) {
         return;
     }
 
-    // transparent box
-    rl.DrawRectangle(box.src.x, box.src.y, box.dst.x - box.src.x, box.dst.y - box.src.y, rl.Fade(rl.BLUE, 0.5));
+    const x = @min(box.src.x, box.dst.x);
+    const y = @min(box.src.y, box.dst.y);
+    const width = @max(box.src.x, box.dst.x) - @min(box.src.x, box.dst.x);
+    const height = @max(box.src.y, box.dst.y) - @min(box.src.y, box.dst.y);
+
+    if (width == 0 or height == 0) {
+        return;
+    }
+
+    rl.DrawRectangle(x, y, width, height, rl.Fade(rl.BLUE, 0.5));
 }
 
 // draw transparent selection box
