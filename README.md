4ba302f62357d802df3b143c597344691271c2f1
Ibrahim Musaddequr Rahman <iamr@umich.edu>
2026-01-03 21:35:18 -0500

    glm 4.7 attempts at webbuild, failed

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # rts-engine
+
+## Building
+
+### Desktop
+
+Run native build:
+```bash
+zig build run
+```
+
+### WebAssembly (WASM)
+
+Build for web:
+```bash
+zig build -Dtarget=wasm32-emscripten web
+```
+
+Build and run locally with emrun:
+```bash
+zig build -Dtarget=wasm32-emscripten run-web
+```
+
+Output will be in `zig-out/web/` directory.
+
+**Note**: First web build will automatically download and install Emscripten SDK (approx. 500MB).
--- build.zig
+++ build.zig
@@ -1,65 +1,31 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+const WebOutputDir = "web";
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
-
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
+fn buildDesktop(b: *std.Build, target: anytype, optimize: std.builtin.OptimizeMode) void {
     const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // We will also create a module for our other entry point, 'main.zig'.
     const exe_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
     exe_mod.addImport("rts_engine_lib", lib_mod);
 
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "rts_engine",
         .root_module = lib_mod,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
     const exe = b.addExecutable(.{
         .name = "rts_engine",
         .root_module = exe_mod,
@@ -71,38 +37,18 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.root_module.linkLibrary(raylibDep.artifact("raylib"));
-
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
@@ -115,10 +61,72 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    if (target.query.os_tag == .emscripten) {
+        const emsdk = b.lazyDependency("emsdk", .{});
+
+        if (emsdk) |dep| {
+            const raylibDep = b.dependency("raylib", .{
+                .target = target,
+                .optimize = optimize,
+            });
+
+            const exe_lib = b.addLibrary(.{
+                .name = "rts_engine",
+                .linkage = .static,
+                .root_module = b.createModule(.{
+                    .target = target,
+                    .optimize = optimize,
+                }),
+            });
+
+            exe_lib.linkLibrary(raylibDep.artifact("raylib"));
+
+            const emsdk_path = dep.path("upstream/emscripten").getPath(b);
+            const emcc_exe = switch (builtin.os.tag) {
+                .windows => "emcc.bat",
+                else => "emcc",
+            };
+
+            const install_dir: std.Build.InstallDir = .{ .custom = WebOutputDir };
+            const output_path = b.getInstallPath(install_dir, "index.html");
+
+            const emcc_command = b.addSystemCommand(&[_][]const u8{b.pathJoin(&.{ emsdk_path, emcc_exe })});
+
+            emcc_command.addArgs(&[_][]const u8{
+                "-o",                                                                output_path,
+                "-sFULL-ES3=1",                                                      "-sUSE_GLFW=3",
+                "-sSTACK_OVERFLOW_CHECK=1",                                          "-sALLOW_MEMORY_GROWTH=1",
+                "-sEXPORTED_RUNTIME_METHODS=['requestFullscreen','exitFullscreen']", "--emrun",
+                "-O" ++ switch (optimize) {
+                    .Debug => "0",
+                    .ReleaseSafe => "2",
+                    .ReleaseFast => "3",
+                    .ReleaseSmall => "s",
+                },
+                "--shell-file",
+                b.path("shell.html").getPath(b),
+            });
+
+            emcc_command.addFileArg(exe_lib.getEmittedBin());
+            emcc_command.addFileArg(raylibDep.artifact("raylib").getEmittedBin());
+
+            const web_step = b.step("web", "Build for web");
+            web_step.dependOn(&emcc_command.step);
+
+            b.getInstallStep().dependOn(&emcc_command.step);
+        } else {
+            std.debug.print("Emscripten SDK not found. Please run: zig build --fetch\n", .{});
+        }
+    } else {
+        buildDesktop(b, target, optimize);
+    }
 }
