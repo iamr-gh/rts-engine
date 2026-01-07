efad3eeee1471e25d7543c800369a2d9dcbfd719
Ibrahim Musaddequr Rahman <iamr@umich.edu>
2026-01-07 00:45:39 -0500

    got partial function application working with hardcoded arity switch

--- src/utils.zig
+++ src/utils.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
-fn concatTuples(comptime a: anytype, comptime b: anytype) concatType(@TypeOf(a), @TypeOf(b)) {
+fn concatTuples(a: anytype, b: anytype) concatType(@TypeOf(a), @TypeOf(b)) {
     const T1 = @TypeOf(a);
     const T2 = @TypeOf(b);
-    const len1 = @typeInfo(T1).Struct.fields.len;
-    const len2 = @typeInfo(T2).Struct.fields.len;
+    const len1 = @typeInfo(T1).@"struct".fields.len;
+    const len2 = @typeInfo(T2).@"struct".fields.len;
 
     var result: concatType(T1, T2) = undefined;
 
@@ -16,8 +16,8 @@ fn concatTuples(comptime a: anytype, comptime b: anytype) concatType(@TypeOf(a),
 
 /// Helper to calculate the result type separately
 fn concatType(comptime T1: type, comptime T2: type) type {
-    const fields1 = @typeInfo(T1).Struct.fields;
-    const fields2 = @typeInfo(T2).Struct.fields;
+    const fields1 = @typeInfo(T1).@"struct".fields;
+    const fields2 = @typeInfo(T2).@"struct".fields;
 
     var types: [fields1.len + fields2.len]type = undefined;
     inline for (fields1, 0..) |f, i| types[i] = f.type;
@@ -26,7 +26,53 @@ fn concatType(comptime T1: type, comptime T2: type) type {
     return std.meta.Tuple(&types);
 }
 
-// partial function application
+fn makeFunc(comptime T: type, comptime f: anytype, comptime provided: anytype) T {
+    const info = @typeInfo(T);
+    const Fn = info.@"fn";
+    const params = Fn.params;
+
+    // have to hardcode arity in funciton genration due to stricter anytype checking in zig .15
+    return switch (params.len) {
+        0 => struct {
+            fn wrapper() Fn.return_type.? {
+                std.debug.print("Called with 0 args\n", .{});
+                return @call(.auto, f, provided);
+            }
+        }.wrapper,
+        1 => struct {
+            fn wrapper(a: params[0].type.?) Fn.return_type.? {
+                const in = concatTuples(provided, .{a});
+                return @call(.auto, f, in);
+            }
+        }.wrapper,
+        2 => struct {
+            fn wrapper(a: params[0].type.?, b: params[1].type.?) Fn.return_type.? {
+                const in = concatTuples(provided, .{ a, b });
+                return @call(.auto, f, in);
+            }
+        }.wrapper,
+        3 => struct {
+            fn wrapper(a: params[0].type.?, b: params[1].type.?, c: params[2].type.?) Fn.return_type.? {
+                const in = concatTuples(provided, .{ a, b, c });
+                return @call(.auto, f, in);
+            }
+        }.wrapper,
+        4 => struct {
+            fn wrapper(a: params[0].type.?, b: params[1].type.?, c: params[2].type.?, d: params[3].type.?) Fn.return_type.? {
+                const in = concatTuples(provided, .{ a, b, c, d });
+                return @call(.auto, f, in);
+            }
+        }.wrapper,
+        5 => struct {
+            fn wrapper(a: params[0].type.?, b: params[1].type.?, c: params[2].type.?, d: params[3].type.?, e: params[4].type.?) Fn.return_type.? {
+                const in = concatTuples(provided, .{ a, b, c, d, e });
+                return @call(.auto, f, in);
+            }
+        }.wrapper,
+        else => @compileError("Arity too high! Add more cases to the switch."),
+    };
+}
+
 fn partialType(comptime args: type, comptime f: type) type {
     const args_info = @typeInfo(args);
     if (args_info != .@"struct") {
@@ -44,37 +90,11 @@ fn partialType(comptime args: type, comptime f: type) type {
 }
 
 pub fn partial(comptime args: anytype, comptime f: anytype) partialType(@TypeOf(args), @TypeOf(f)) {
-    // const args_type = @TypeOf(args);
-    // const args_info = @typeInfo(args);
-
-    const output_f_type = @typeInfo(partialType(@TypeOf(args), @TypeOf(f)));
-    const output_f_params = output_f_type.@"fn".params;
-
-    var param_types: [output_f_params.len]type = undefined;
-    for (output_f_params, 0..) |param, i| {
-        param_types[i] = param.type.?;
-    }
-
-    const Args = std.meta.Tuple(&param_types);
-
-    // const f_info = @typeInfo(f);
-    const output_t: type = output_f_type.@"fn".return_type.?;
-
-    return struct {
-        pub fn call(user_args: Args) output_t {
-            // this generates the callback?
-            inline for (output_f_params, 0..) |param, i| {
-                std.debug.print("Arg {d} ({}) = {any}\n", i, param.type.?, args[i]);
-            }
-
-            // need to create a merged structure based on the inputted params and the user_args
-            const total_args = concatTuples(args, user_args);
-            @call(.auto, f, total_args);
-        }
-    }.call;
+    const out_t = partialType(@TypeOf(args), @TypeOf(f));
+    return makeFunc(out_t, f, args);
 }
 
-pub fn condNum(b: bool, a: i32) i32 {
+fn condNum(b: bool, a: i32) i32 {
     if (b) {
         return a;
     } else {
@@ -82,13 +102,16 @@ pub fn condNum(b: bool, a: i32) i32 {
     }
 }
 
-pub fn num(a: i32) i32 {
+fn num(a: i32) i32 {
     return a;
 }
 
 test "partialApplication" {
     // type propagation
     std.debug.assert(partialType(@TypeOf(.{false}), @TypeOf(condNum)) == @TypeOf(num));
+
+    const num1 = partial(.{1}, num);
+    std.debug.assert(num1() == 1);
 
     // partial application
     const condTrue = partial(.{true}, condNum);
